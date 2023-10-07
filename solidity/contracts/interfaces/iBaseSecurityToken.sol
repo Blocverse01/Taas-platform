2078e0bc08fcb9dcf1914840978ae04e8fe594eb
king-Alex-d-great <ogubuikealex@gmail.com>
2023-10-07 18:31:05 +0100

    Chore: Remove redundant contract inheritance

--- solidity/contracts/interfaces/iBaseSecurityToken.sol
+++ solidity/contracts/interfaces/iBaseSecurityToken.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-import "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 
-interface IBaseSecurityToken is IERC20Upgradeable {
+interface IBaseSecurityToken {
     // Token Issuance
     function isIssuable() external view returns (bool);
 
