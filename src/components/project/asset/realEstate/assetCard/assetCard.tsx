84956006d3714fd4f3924349d08f208dd82be2bf
Avoaja Joshua Akachi <avoajajoshua@gmail.com>
2023-10-13 13:37:33 +0100

    feat: base code for asset card

---
+++ src/components/project/asset/realEstate/assetCard/assetCard.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+interface RealEstateAssetCardProps {}
+
+const RealEstateAssetCard: FC<RealEstateAssetCardProps> = () => {
+  return <div></div>;
+};
+
+export { RealEstateAssetCard };
