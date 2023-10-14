4a5fc32fc72c02b9183e6f3f6ab1015031b8e665
Avoaja Joshua Akachi <avoajajoshua@gmail.com>
2023-10-14 14:55:16 +0000

    feat: add new colors and animations

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -26,8 +26,28 @@ const config: Config = {
             2: "#FAFAFA",
             3: "#464545",
             4: "#646464",
+            5: "#474747",
           },
         },
+      },
+      keyframes: {
+        overlayShow: {
+          from: { opacity: "0" },
+          to: { opacity: "1" },
+        },
+        contentShow: {
+          from: { opacity: "0", transform: "translate(-50%, -48%) scale(0.96)" },
+          to: { opacity: "1", transform: "translate(-50%, -50%) scale(1)" },
+        },
+        slideUpAndFade: {
+          from: { opacity: "0", transform: "translateY(2px)" },
+          to: { opacity: "1", transform: "translateY(0)" },
+        },
+      },
+      animation: {
+        overlayShow: "overlayShow 150ms cubic-bezier(0.16, 1, 0.3, 1)",
+        contentShow: "contentShow 150ms cubic-bezier(0.16, 1, 0.3, 1)",
+        slideUpAndFade: "slideUpAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)",
       },
     },
   },
