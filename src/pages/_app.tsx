7cd6b2f19f826315e679387153d4bb9c1a2fd478
Avoaja Joshua Akachi <avoajajoshua@gmail.com>
2023-10-14 15:09:42 +0000

    feat: add modal parent to App

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,22 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { aeonikFont } from "@/font/setup";
+import "react-loading-skeleton/dist/skeleton.css";
+import { useEffect, useRef } from "react";
+import { useSetModalParent } from "@/lib/zustand/modalSlice";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const modalRef = useRef<HTMLDivElement | null>(null);
+  const setModalParent = useSetModalParent();
+
+  useEffect(() => {
+    if (modalRef.current) {
+      setModalParent(modalRef.current);
+    }
+  }, [setModalParent]);
+
   return (
-    <div className={`${aeonikFont.variable} font-aeonik`}>
+    <div ref={modalRef} className={`${aeonikFont.variable} font-aeonik`}>
       <Component {...pageProps} />
     </div>
   );
