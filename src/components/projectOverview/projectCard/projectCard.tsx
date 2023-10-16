6fed386cdb5a27b10e2fd138bbe8c4ad63b9aef9
Logan <tushlogan@gmail.com>
2023-10-16 20:12:34 +0100

    fix: svg color change on hover

--- src/components/projectOverview/projectCard/projectCard.tsx
+++ src/components/projectOverview/projectCard/projectCard.tsx
@@ -2,9 +2,10 @@
 import { Caret, House } from "@/components/icons";
 import Image from "next/image";
 import Link from "next/link";
-import { FC, ReactElement } from "react";
+import { FC, ReactElement, useState } from "react";
 import { formatDistance } from "date-fns";
 import CardBg from "@/assets/project-card-bg.png";
+import { motion } from "framer-motion";
 
 interface ProjectCardProps {
   project: {
@@ -16,12 +17,22 @@ interface ProjectCardProps {
   };
 }
 
-//  TODO: make SVG change color on hover
-
 const ProjectCard: FC<ProjectCardProps> = ({ project }) => {
   const { name, assetType, timeCreated, link: projectLink } = project;
+  const [isHovered, setIsHovered] = useState(false);
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
   return (
-    <div className="bg-t-gray overflow-hidden  text-t-purple duration-200 hover:scale-105 hover:bg-t-purple hover:text-white  w-full h-[181px]  rounded-[10px] border border-[#ECECEC] flex flex-col space-y-4  p-5 relative group">
+    <div
+      className="bg-t-gray overflow-hidden  text-t-purple duration-200 hover:scale-105 hover:bg-t-purple hover:text-white  w-full h-[181px]  rounded-[10px] border border-[#ECECEC] flex flex-col space-y-4  p-5 relative group"
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
       <Image
         className="absolute  scale-110   z-1 inset-0  group-hover:opacity-10"
         src={CardBg}
@@ -30,7 +41,7 @@ const ProjectCard: FC<ProjectCardProps> = ({ project }) => {
       <Link href={projectLink} className="absolute z-[999] inset-0" />
       <div className="flex z-10 items-center justify-between">
         <div className="group-hover:text-white">
-          <House />
+          <House fill={isHovered ? "#FFFFFF" : "#371B89"} />
         </div>
         <p className="  text-[12px] font-medium text-t-purple group-hover:text-t-purple2 capitalize  bg-t-purple3 duration-200 group-hover:bg-white  px-4 py-1 rounded-full  ">
           {assetType}
@@ -40,7 +51,7 @@ const ProjectCard: FC<ProjectCardProps> = ({ project }) => {
         <p className=" text-[20px] text-t-purple4 duration-200 group-hover:text-white truncate max-w-[165px] ">
           {name}
         </p>
-        <Caret />
+        <Caret fill={isHovered ? "#FFFFFF" : "#371B89"} />
       </div>
       <div className="h-[2px] w-full z-10 bg-[#DADADA]"></div>
       <p className="z-10 text-[12px] ">
