ba25512a9f766f343ce0eee82ccd155fd79143f5
Avoaja Joshua Akachi <avoajajoshua@gmail.com>
2023-10-18 22:49:41 +0100

    feat: add middleware for protected routes

---
+++ src/middleware.ts
@@ -0,0 +1,26 @@
+import { NextRequest, NextResponse } from "next/server";
+import { getToken } from "next-auth/jwt";
+
+const AUTH_ROUTES = ["/login", "/signup"];
+
+export async function middleware(request: NextRequest) {
+  const token = await getToken({ req: request });
+  
+  let redirect: string | undefined;
+
+  const isAuthenticationRoute = AUTH_ROUTES.includes(request.nextUrl.pathname);
+
+  if (token && isAuthenticationRoute) {
+    redirect = "/dashboard";
+  }
+  if (!token && !isAuthenticationRoute) {
+    redirect = "/login";
+  }
+
+  const response = redirect
+    ? NextResponse.redirect(new URL(redirect, request.url))
+    : NextResponse.next();
+  return response;
+}
+
+export const config = { matcher: ["/dashboard/:path*", "/login", "/signup"] };
