b87c47eea365d70ec99c314bf6e13f85b11b58e9
king-Alex-d-great <ogubuikealex@gmail.com>
2023-10-18 07:33:47 +0100

    FeatL Add mail option for add new teammate

---
+++ src/utils/email/helpers.ts
@@ -0,0 +1,25 @@
+
+
+
+type AddTeamMemberEmailPayload = {
+    firstname: string;
+    email: string;
+    senderEmail: string;
+    link: string
+}
+
+export function getAddTeamMemberMailOption({ firstname, email, link, senderEmail }: AddTeamMemberEmailPayload) {
+    let body = `
+  <h3>Hey ${firstname}!</h3>
+  <p>${senderEmail} would love to have you on his TAAS project</p>
+  <p><a href="${link}">Check out</a> this invitation now!</p>
+  <br /> 
+  <p>Team TAAS</p>`;
+    return {
+        body,
+        subject: "Join TAAS Project",
+        to: email,
+        html: body,
+        from: process.env.EMAIL_ADDRESS,
+    };
+};
