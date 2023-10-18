4f301b85204b9e9f3d64867ceb3291c64761b4e8
king-Alex-d-great <ogubuikealex@gmail.com>
2023-10-18 13:13:52 +0100

    Feat: Abstract project team types

---
+++ src/lib/taas-api/team/teamTypes.ts
@@ -0,0 +1,18 @@
+
+export type AddNewMemberPayload = {
+    projectId: string,
+    name: string,
+    email: string,
+    role: string
+}
+
+export type updateMemberRolePayload = {
+    projectId: string,
+    teamMemberUserId: string,    
+    newRole: string
+}
+
+export type deactivateMemberRolePayload = {
+    projectId: string,
+    teamMemberUserId: string,    
+}
