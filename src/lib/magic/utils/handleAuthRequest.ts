add6fb5f77f33998c794ab15e10519366cce5674
Avoaja Joshua Akachi <avoajajoshua@gmail.com>
2023-10-18 23:00:22 +0100

    feat: implement magic auth utils

---
+++ src/lib/magic/utils/handleAuthRequest.ts
@@ -0,0 +1,31 @@
+import { userRepository } from "@/utils/constants";
+import { validateDIDToken } from "./validateDIDToken";
+import { createUser } from "@/lib/taas-api/user";
+
+interface AuthenticatedUser {
+  id: string;
+  email: string;
+  walletAddress: string;
+}
+
+const handleAuthRequest = async (
+  DIDToken: string
+): Promise<AuthenticatedUser> => {
+  const magicUser = await validateDIDToken(DIDToken);
+  let dbUser = await userRepository().read(magicUser.issuer);
+
+  if (!dbUser) {
+    dbUser = await createUser({
+      email: magicUser.email,
+      walletAddress: magicUser.publicAddress,
+    });
+  }
+
+  return {
+    id: dbUser.id,
+    email: dbUser.email!,
+    walletAddress: dbUser.walletAddress!,
+  };
+};
+
+export { handleAuthRequest };
