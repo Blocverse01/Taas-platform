657d450cf4578a1fbf971a4dc8feccee7244ee74
king-Alex-d-great <ogubuikealex@gmail.com>
2023-10-18 21:09:11 +0100

    Feat: Move hardhat config file

---
+++ hardhat.config.ts
@@ -0,0 +1,29 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.20",
+  paths: {
+    sources: "./solidity/contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./solidity/artifacts"
+  },
+  networks: {
+    mumbai: {
+      url: `https://rpc.ankr.com/polygon_mumbai`,
+      accounts: [`${process.env.SECRET_KEY}`],
+    },
+    optimism: {
+      url: `https://rpc.ankr.com/optimism_testnet`,
+      accounts: [`${process.env.SECRET_KEY}`],
+    },
+    scroll: {
+      url: `https://rpc.ankr.com/scroll_sepolia_testnet`,
+      accounts: [`${process.env.SECRET_KEY}`],
+    }
+  },
+};
+
+export default config;
+//
