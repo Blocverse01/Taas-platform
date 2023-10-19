310f1ea3b0d9906ec75292ca354e498b9838c4f2
king-Alex-d-great <ogubuikealex@gmail.com>
2023-10-19 07:25:48 +0100

    Fix: Add current session user

--- src/lib/safe/getSafeDetails.ts
+++ src/lib/safe/getSafeDetails.ts
@@ -1,12 +1,17 @@
 import { Address } from "viem"
 import { getSafe } from "./safeSdk";
 import { getTeamMembers } from "../taas-api/team/getMembers";
+import { getSession } from "next-auth/react";
+import { Session } from "next-auth";
 
 const getSafeDetails = async (safeAddress: Address) => {
   //Retrieve the safe SDK for the safeAddress using the currenlty logged in user as a signer
   const safe = await getSafe(safeAddress);
-//
-  const teamMembers = await getTeamMembers(curr,'sample-team-id');
+
+  const currentUser = await getSession() as Session;
+
+  const teamMembers = await getTeamMembers(currentUser, 'sample-team-id');
+  
   //retrieve the owners of the safe address passed in
   const owners = await safe.getOwners();
 
