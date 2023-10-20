d19ab24641410d2fd231af998f7ecbcf8f706239
king-Alex-d-great <ogubuikealex@gmail.com>
2023-10-20 03:36:29 +0100

    Chore: Update required properties from teammembers

--- src/lib/taas-api/team/getMembers.ts
+++ src/lib/taas-api/team/getMembers.ts
@@ -7,14 +7,20 @@ const getTeamMembers = async (currentUser: Session, teamId: string) => {
     const dummyMembers = [{
       email: "toochukwukingz6@gmail.com",
       walletAddress: "0x2525D2694A862Bb03e143Ad8B10695177EBC6CF0",
+      firstname: "",
+      userId: ""
     },
     {
       email: "adelekekehinde06@gmail.com",
-      walletAddress: "0x4AA4b9EAF070DBeDBF1ED7709e330C2118C7CF16"
+      walletAddress: "0x4AA4b9EAF070DBeDBF1ED7709e330C2118C7CF16",
+      firstname: "",
+      userId: ""
     },
     {
       email: "avoajajoshua@gmail.com",
-      walletAddress: "0xaBeDc0A70237A82482F4c6E48623005d80E2b95C"
+      walletAddress: "0xaBeDc0A70237A82482F4c6E48623005d80E2b95C",
+      firstname: "",
+      userId: ""
     }]
     return dummyMembers.map((member) => ({
       ...member,
