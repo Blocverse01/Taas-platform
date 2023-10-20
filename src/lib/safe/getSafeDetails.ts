f332ef52e63e799acf0f7befa14d2871df6e363e
Avoaja Joshua Akachi <avoajajoshua@gmail.com>
2023-10-20 16:05:20 +0100

    fix: pass correct obj to getTeamMembers

--- src/lib/safe/getSafeDetails.ts
+++ src/lib/safe/getSafeDetails.ts
@@ -2,16 +2,18 @@ import { Address } from "viem"
 import { getSafe } from "./safeSdk";
 import { getTeamMembers } from "../taas-api/team/getMembers";
 import { getSession } from "next-auth/react";
-import { Session } from "next-auth";
+import type { Session } from "next-auth";
 
 const getSafeDetails = async (safeAddress: Address) => {
   //Retrieve the safe SDK for the safeAddress using the currenlty logged in user as a signer
   const safe = await getSafe(safeAddress);
 
-  const currentUser = await getSession() as Session;
+  const currentUser = await getSession();
 
-  const teamMembers = await getTeamMembers(currentUser, 'sample-team-id');
-  
+  if (!currentUser) throw new Error("No auth session");
+
+  const teamMembers = await getTeamMembers(currentUser.user, 'sample-team-id');
+
   //retrieve the owners of the safe address passed in
   const owners = await safe.getOwners();
 
