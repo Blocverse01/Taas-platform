ee10eaa04f375a75ab514a47af7c709f664a6c49
king-Alex-d-great <ogubuikealex@gmail.com>
2023-10-20 03:33:17 +0100

    Feat: Create enums for assetTransaction and authRequest

---
+++ src/utils/enums.ts
@@ -0,0 +1,15 @@
+export enum AuthorizationRequest {
+    Pending = 1,
+    Accepted = 2,
+    Rejected = 3
+}
+
+export enum AssetTransactionType {
+    IssueToken = 1
+}
+
+export enum AssetTransactionStatus {
+    Pending = 1,
+    Accepted = 2,
+    Rejected = 3
+}
