726dbfd34f43be9e462c612e1969c07fc6ca321a
king-Alex-d-great <ogubuikealex@gmail.com>
2023-10-20 03:33:44 +0100

    Feat: Add mail options for authorization request

--- src/utils/email/helpers.ts
+++ src/utils/email/helpers.ts
@@ -6,6 +6,14 @@ type AddTeamMemberEmailPayload = {
     link: string
 }
 
+type AuthorizationRequestPayload = {
+    firstname: string,
+    link: string,
+    projectName: string,
+    assetName: string,
+    email: string
+}
+
 export function getAddTeamMemberMailOption({ firstname, email, link, senderEmail }: AddTeamMemberEmailPayload) {
     let body = `
   <h3>Hey ${firstname}!</h3>
@@ -17,6 +25,23 @@ export function getAddTeamMemberMailOption({ firstname, email, link, senderEmail
         body,
         subject: "Join TAAS Project",
         to: email,
+        html: body,
+        from: process.env.EMAIL_ADDRESS,
+    };
+};
+
+export function getAuthorizationRequestMailOption(payload: AuthorizationRequestPayload) {
+    let body = `
+  <h3>Hello ${payload.firstname}!</h3>
+  <p>Your team member created an issue token request for project: ${payload.projectName}, asset: ${payload.assetName}</p>
+  <p>Your authorization is required for the request to be granted</p>
+  <p><a href="${payload.link}">View this authorization invite!</a></p>
+  <br /> 
+  <p>Team TAAS</p>`;
+    return {
+        body,
+        subject: "Authorization Invite",
+        to: payload.email,
         html: body,
         from: process.env.EMAIL_ADDRESS,
     };
