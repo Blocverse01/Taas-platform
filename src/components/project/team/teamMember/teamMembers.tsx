a266bd134364e0dea0cf7cfbabd6b7d8f0972d25
Avoaja Joshua Akachi <avoajajoshua@gmail.com>
2023-10-20 15:59:07 +0100

    feat: correct color utility

--- src/components/project/team/teamMember/teamMembers.tsx
+++ src/components/project/team/teamMember/teamMembers.tsx
@@ -50,7 +50,7 @@ const TeamMembers: FC<TeamMembersProps> = ({ teamMembers }) => {
                   </Popover.Trigger>
                   <Popover.Portal>
                     <Popover.Content
-                      className="rounded p-5 flex flex-col  bg-white shadow-[0_10px_38px_-10px_hsla(206,22%,7%,.35),0_10px_20px_-15px_hsla(206,22%,7%,.2)] focus:shadow-[0_10px_38px_-10px_hsla(206,22%,7%,.35),0_10px_20px_-15px_hsla(206,22%,7%,.2),0_0_0_2px_theme(colors.violet7)] will-change-[transform,opacity] data-[state=open]:data-[side=top]:animate-slideDownAndFade data-[state=open]:data-[side=right]:animate-slideLeftAndFade data-[state=open]:data-[side=bottom]:animate-slideUpAndFade data-[state=open]:data-[side=left]:animate-slideRightAndFade"
+                      className="rounded p-5 flex flex-col  bg-white shadow-[0_10px_38px_-10px_hsla(206,22%,7%,.35),0_10px_20px_-15px_hsla(206,22%,7%,.2)] focus:shadow-[0_10px_38px_-10px_hsla(206,22%,7%,.35),0_10px_20px_-15px_hsla(206,22%,7%,.2),0_0_0_2px_theme(colors.t.black)] will-change-[transform,opacity] data-[state=open]:data-[side=top]:animate-slideDownAndFade data-[state=open]:data-[side=right]:animate-slideLeftAndFade data-[state=open]:data-[side=bottom]:animate-slideUpAndFade data-[state=open]:data-[side=left]:animate-slideRightAndFade"
                       sideOffset={1}
                     >
                       <RemoveTeamMemberDialog />
