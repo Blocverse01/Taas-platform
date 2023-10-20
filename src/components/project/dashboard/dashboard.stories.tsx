e6167778b1dd4c94e8f97292dcdb75894ec8b506
Avoaja Joshua Akachi <avoajajoshua@gmail.com>
2023-10-20 15:47:32 +0100

    fix: correct storybook args

--- src/components/project/dashboard/dashboard.stories.tsx
+++ src/components/project/dashboard/dashboard.stories.tsx
@@ -1,10 +1,17 @@
 import { Meta, StoryObj } from "@storybook/react";
 import { ProjectDashboard } from "./dashboard";
+import { getDummyProjectAnalytics } from "./demoData";
 
 const meta = {
   title: "Project/Dashboard",
   component: ProjectDashboard,
   tags: ["autodocs"],
+  args: {
+    projectId: "dummy-project",
+    getAnalytics(projectId) {
+      return getDummyProjectAnalytics(projectId);
+    },
+  },
 } satisfies Meta<typeof ProjectDashboard>;
 
 export default meta;
