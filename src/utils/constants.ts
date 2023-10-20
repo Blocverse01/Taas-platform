54d702edc510f8eedde01d336031e098003cfec4
king-Alex-d-great <ogubuikealex@gmail.com>
2023-10-20 03:34:48 +0100

    Feat: Add repository constants for asset, and authrequest

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,5 +2,7 @@ import { getXataClient } from "@/xata";
 
 export const userRepository = () => getXataClient().db.User;
 export const projectRepository = () => getXataClient().db.Project;
-export const projectTeamRepository = () =>
-  getXataClient().db.ProjectTeamMembers;
+export const projectTeamRepository = () => getXataClient().db.ProjectTeamMembers;
+export const assetTransactionRepository = () => getXataClient().db.AssetTransaction;
+export const assetPropertyRepository = () => getXataClient().db.TokenizedProperty;
+export const authorizationRequestRepository = () => getXataClient().db.AuthorizationRequest;
