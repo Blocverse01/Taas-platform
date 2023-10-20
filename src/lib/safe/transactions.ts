15aa2854d022ae3afc73a13e1e1df710484d22ac
king-Alex-d-great <ogubuikealex@gmail.com>
2023-10-20 12:51:31 +0100

    Feat: Create propose transaction

--- src/lib/safe/transactions.ts
+++ src/lib/safe/transactions.ts
@@ -1,7 +1,8 @@
 import { Address } from "viem"
-import { AddOwnerTxParams, RemoveOwnerTxParams } from "@safe-global/protocol-kit";
-import { SafeTransaction } from "@safe-global/safe-core-sdk-types";
+import Safe, { AddOwnerTxParams, RemoveOwnerTxParams } from "@safe-global/protocol-kit";
+import { SafeTransaction, SafeTransactionDataPartial } from "@safe-global/safe-core-sdk-types";
 import { validateSignatory } from "./validateSafeSigner";
+import { getSafeService } from ".";
 
 export const createRemoveSignatoryTransaction =
     async (safeAddress: Address, params: RemoveOwnerTxParams): Promise<SafeTransaction> => {
@@ -25,6 +26,34 @@ export const createUpdateSafeWalletThreshold =
 
         return await safe.createChangeThresholdTx(newThreshold);
     }
+
+export const proposeTransaction = async (
+    safeTransactionData: SafeTransactionDataPartial,
+    safe: Safe,
+    safeAddress: Address,
+    senderAddress: Address) => {
+
+    // Create a Safe transaction with the provided parameters
+    const safeTransaction = await safe.createTransaction({ safeTransactionData });
+
+    const nonce = safeTransaction.data.nonce;
+
+    // Deterministic hash based on transaction parameters
+    const safeTxHash = await safe.getTransactionHash(safeTransaction)
+
+    // Sign transaction to verify that the transaction is coming from owner 1
+    const senderSignature = await safe.signTransactionHash(safeTxHash)
+
+    await getSafeService().proposeTransaction({
+        safeAddress,
+        safeTransactionData: safeTransaction.data,
+        safeTxHash,
+        senderAddress,
+        senderSignature: senderSignature.data,
+    });
+
+    return { safeTxHash, nonce };
+}
 
 
 //TODO : BATCH REMOVAL
