f9d8019aba8963925bd32d97c2eccdfe72b0d909
Avoaja Joshua Akachi <avoajajoshua@gmail.com>
2023-10-20 15:39:50 +0100

    feat: add helper function to generate page title

--- src/utils/helperfunctions.ts
+++ src/utils/helperfunctions.ts
@@ -2,3 +2,14 @@
 export const getProjectTeamMemberId = (projectId: string, userId: string) => {
     return `${projectId}-${userId}`
 }
+
+export const derivePageTitle = (routerPath: string, projectId: string) => {
+    const projectsSplit = routerPath.split(projectId);
+    const projectRouteSplit = projectsSplit[1].split("/");
+
+    if (projectRouteSplit.length === 1) return "dashboard";
+
+    if (projectRouteSplit.length >= 2) return projectRouteSplit[1];
+
+    return "";
+};
