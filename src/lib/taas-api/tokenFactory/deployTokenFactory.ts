458c882a0071218152b571c013e87a07a7362b9b
Avoaja Joshua Akachi <avoajajoshua@gmail.com>
2023-10-21 20:59:52 +0100

    feat: add token factory functionalities

--- src/lib/taas-api/tokenFactory/deployTokenFactory.ts
+++ src/lib/taas-api/tokenFactory/deployTokenFactory.ts
@@ -1,49 +1,39 @@
 import {
   Address,
+  TransactionReceipt as PreciseTransactionReceipt,
   decodeEventLog,
   encodeFunctionData,
-  TransactionReceipt as PreciseTransactionReceipt,
-  hashMessage
 } from "viem";
-import { TOKEN_FACTORY, PLATFORM_ENTRY } from "@/utils/web3/abis";
 import {
   getAccount,
   getPublicClient,
   getWalletClient,
 } from "@/utils/web3/connection";
-import { utils } from "@/utils/web3/utils";
+import { PLATFORM_ENTRY } from "@/utils/web3/abis";
 import { getContractAddress } from "@/utils/web3/contracts";
 import { sponsorTransaction } from "@/lib/biconomy";
 import { SPONSOR_TRANSACTION } from "@/utils/constants";
 
-const CONTRACT_FUNCTION_NAME = "createToken" as const;
-const INTER_CHAIN_TOKEN_SERVICE = "0xF786e21509A9D50a9aFD033B5940A2b7D872C208" as const;
+const CONTRACT_FUNCTION_NAME = "createTokenFactory" as const;
 
-interface TokenizeAssetResponse {
-  tokenAddress: Address;
+interface DeployFactoryResponse {
+  tokenFactory: Address;
   txHash: Address;
 }
 
-const tokenizeAsset = async (
-  tokenFactory: Address,
-  tokenTicker: string,
-  tokenPrice: number,
-  tokenName: string
-): Promise<TokenizeAssetResponse> => {
+const deployTokenFactory = async (
+  multiSigSafeAddress: Address,
+  treasuryAddress: Address
+): Promise<DeployFactoryResponse> => {
   const account = await getAccount();
-  const publicClient = getPublicClient();
-  const walletClient = getWalletClient();
   const platformEntryAddress = getContractAddress("PLATFORM_ENTRY");
-
-  const salt = hashMessage(tokenName);
+  const walletClient = getWalletClient();
+  const publicClient = getPublicClient();
 
   const functionArgs = [
-    tokenFactory,
-    salt,
-    INTER_CHAIN_TOKEN_SERVICE,
-    tokenName,
-    tokenTicker,
-    utils.parseEther(`${tokenPrice}`),
+    multiSigSafeAddress,
+    multiSigSafeAddress,
+    treasuryAddress,
   ] as const;
 
   if (SPONSOR_TRANSACTION) {
@@ -60,14 +50,14 @@ const tokenizeAsset = async (
     const receipt = await publicClient.getTransactionReceipt({
       hash: txHash,
     });
-    return extractResponseFromReceipt(receipt, tokenFactory);
+    return extractResponseFromReceipt(receipt, platformEntryAddress);
   }
 
   const { request } = await publicClient.simulateContract({
     account,
     address: platformEntryAddress,
     abi: PLATFORM_ENTRY,
-    functionName: "createToken",
+    functionName: CONTRACT_FUNCTION_NAME,
     args: functionArgs,
   });
 
@@ -75,33 +65,31 @@ const tokenizeAsset = async (
   const receipt = await publicClient.waitForTransactionReceipt({
     hash: txHash,
   });
-  return extractResponseFromReceipt(receipt, tokenFactory);
+  return extractResponseFromReceipt(receipt, platformEntryAddress);
 };
 
 const extractResponseFromReceipt = (
   receipt: PreciseTransactionReceipt,
-  tokenFactoryAddress: Address
+  platformEntryAddress: Address
 ) => {
   if (receipt.status === "reverted") throw new Error("Transaction failed");
 
-  console.log(receipt.logs);
-
   const log = receipt.logs.find(
-    (l) => l.address.toLowerCase() === tokenFactoryAddress.toLowerCase()
+    (l) => l.address.toLowerCase() === platformEntryAddress.toLowerCase()
   );
   if (!log) throw new Error("Reference log not found");
 
   const topics = decodeEventLog({
-    eventName: "TokenCreated",
-    abi: TOKEN_FACTORY,
+    eventName: "FactoryCreated",
+    abi: PLATFORM_ENTRY,
     data: log.data,
     topics: log.topics,
   });
   const response = {
-    tokenAddress: topics.args.contractAddress,
+    tokenFactory: topics.args.contractAddress,
     txHash: receipt.transactionHash,
   };
   return response;
 };
 
-export { tokenizeAsset };
+export { deployTokenFactory };
