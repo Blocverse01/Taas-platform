8f750b7f18052092d4703a1452718feb367b0a60
Avoaja Joshua Akachi <avoajajoshua@gmail.com>
2023-10-22 00:38:25 +0100

    feat: add endpoint to handle sending emails

---
+++ src/pages/api/user/send-email.ts
@@ -0,0 +1,40 @@
+import { NextApiHandler, NextApiResponse } from "next";
+import { INTERNAL_SERVER_ERROR } from "@/utils/constants";
+import { validateAuthInApiHandler } from "@/utils/auth";
+import MailService from "@/utils/email";
+import Yup from "yup";
+
+type ApiResponse = {
+    message?: string;
+    data?: boolean;
+};
+
+const mailOptionSchema = Yup.object({
+    body: Yup.string().required(),
+    subject: Yup.string().required(),
+    to: Yup.string().required(),
+    html: Yup.string().required(),
+    from: Yup.string(),
+})
+
+const handler: NextApiHandler = async (req, res: NextApiResponse<ApiResponse>) => {
+    try {
+        await validateAuthInApiHandler(req, res);
+
+        const mailOptions = await mailOptionSchema.validate(req.body);
+
+        (await MailService.getTransporter()).sendMail(mailOptions, (error) => {
+            if (error) {
+                throw new Error("An Error occurred\nNo email sent!");
+            }
+        });
+
+        res.status(200).json({ data: true });
+    } catch (error: any) {
+        return res
+            .status(error?.status ?? error?.response?.status ?? INTERNAL_SERVER_ERROR)
+            .json({ message: error.message });
+    }
+}
+
+export default handler;
