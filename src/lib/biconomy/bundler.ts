a325ec0b9adfc3af6edd230c355033980ba42848
Avoaja Joshua Akachi <avoajajoshua@gmail.com>
2023-10-21 20:56:46 +0100

    feat: add mainnet bundler for biconomy

--- src/lib/biconomy/bundler.ts
+++ src/lib/biconomy/bundler.ts
@@ -5,7 +5,12 @@ import { DEFAULT_ENTRYPOINT_ADDRESS } from "@biconomy/account";
 //Get a chain Id based on the current environment
 const chainId = getViemChain().id;
 
-const BUNDLER_URL = `https://bundler.biconomy.io/api/v2/${chainId}/nJPK7B3ru.dd7f7861-190d-41bd-af80-6877f74b8f44`;
+const BUNDLER_URLS: {
+  [key: number]: string
+} = {
+  80001: `https://bundler.biconomy.io/api/v2/80001/nJPK7B3ru.dd7f7861-190d-41bd-af80-6877f74b8f44`,
+  137: "https://bundler.biconomy.io/api/v2/137/fAPY9R6ts.gg1v9570-521z-59jk-gt69-9654m83c0m37"
+};
 
 let bundler: Bundler | undefined;
 
@@ -14,7 +19,7 @@ const getBundler = () => {
 
   //Creatin a bundler instance if it does not already exist
   bundler = new Bundler({
-    bundlerUrl: BUNDLER_URL,
+    bundlerUrl: BUNDLER_URLS[chainId],
     chainId,
     entryPointAddress: DEFAULT_ENTRYPOINT_ADDRESS,
   });
