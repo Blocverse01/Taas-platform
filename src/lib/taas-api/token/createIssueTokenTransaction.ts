8fdc6c48270fc94fcb316c069c1bcfb634f02914
Avoaja Joshua Akachi <avoajajoshua@gmail.com>
2023-10-21 20:59:10 +0100

    feat: add initial approval step to proposing tx

--- src/lib/taas-api/token/createIssueTokenTransaction.ts
+++ src/lib/taas-api/token/createIssueTokenTransaction.ts
@@ -1,38 +1,31 @@
-import { getSafeService } from "@/lib/safe";
-import { getSafeDetails } from "@/lib/safe/getSafeDetails";
 import { proposeTransaction } from "@/lib/safe/transactions";
 import { validateSignatory } from "@/lib/safe/validateSafeSigner";
-import { ISSUE_TOKEN_CONTRACT_FUNCTION_NAME, ZERO, assetPropertyRepository, assetTransactionRepository, authorizationRequestRepository, projectRepository } from "@/utils/constants";
-import MailService from "@/utils/email";
-import { getAuthorizationRequestMailOption } from "@/utils/email/helpers";
-import { AssetTransactionStatus, AssetTransactionType, AuthorizationRequest } from "@/utils/enums";
-import { getConcatenatedId } from "@/utils/helperfunctions";
+import { ISSUE_TOKEN_CONTRACT_FUNCTION_NAME, assetTransactionRepository, authorizationRequestRepository } from "@/utils/constants";
 import { PLATFORM_ENTRY } from "@/utils/web3/abis";
+import { getContractAddress } from "@/utils/web3/contracts";
 import { utils } from "@/utils/web3/utils";
 import { AddressZero } from "@biconomy/common";
-import Safe from "@safe-global/protocol-kit";
-import { OperationType, SafeTransactionData, SafeTransactionDataPartial } from "@safe-global/safe-core-sdk-types";
+import { OperationType, SafeTransactionDataPartial } from "@safe-global/safe-core-sdk-types";
 import { Address, encodeFunctionData } from "viem";
 
+interface IssueTokenPayload {
+    tokenFactory: Address;
+    tokenAddress: Address;
+    destinationWallet: Address;
+    amount: number;
+}
+
 export const createIssueTokenAssetTransaction = async (
-    projectId: string,
-    tokenId: Address,
-    destinationWallet: Address,
-    amount: number) => {
+    multisigController: Address,
+    payload: IssueTokenPayload) => {
+    const { currentUser, safe } = await validateSignatory(multisigController);
+    const { tokenFactory, tokenAddress, destinationWallet, amount } = payload;
 
-    const asset = await assetPropertyRepository().read(tokenId, ['project.*', "*"]);
-
-    if (!asset || !asset.project) {
-        throw new Error(!asset ? "Invalid token identifier" : "Reference to Invalid project");
-    }
-
-    const { project } = asset;
-
-    const { currentUser, safe } = await validateSignatory(project.multisigController as Address);
+    const platformEntry = getContractAddress("PLATFORM_ENTRY");
 
     const functionArgs = [
-        asset.project.tokenFactory as Address,
-        asset.tokenAddress as Address,
+        tokenFactory,
+        tokenAddress,
         destinationWallet,
         utils.parseEther(`${amount}`),
         AddressZero,
@@ -44,63 +37,28 @@ export const createIssueTokenAssetTransaction = async (
         args: functionArgs
     });
 
-    //propose transaction on safe
+    // propose transaction on safe
     const safeTransactionData: SafeTransactionDataPartial = {
         operation: OperationType.Call,
         data: encodedData,
-        to: asset.tokenAddress,
-        value: ""
+        to: platformEntry,
+        value: "0"
     };
 
-    const {safeTxHash, nonce} = await proposeTransaction(
+    const { safeTxHash, nonce } = await proposeTransaction(
         safeTransactionData,
         safe,
-        project.multisigController as Address,
-        currentUser.user.walletAddress as Address
+        multisigController,
+        currentUser.user.walletAddress
     );
 
-    //Create a transaction on xata for the token
-    const transaction = await assetTransactionRepository().create({
-        asset: tokenId,
-        status: AssetTransactionStatus.Pending,
-        type: AssetTransactionType.IssueToken,
-        createdBy: currentUser.user.id,
-        amount,
-        safeTransactionHash: safeTxHash,
-        safeTransactionNonce : nonce
-    });
+    const txResponse = await safe.approveTransactionHash(safeTxHash)
 
-    //get authorizers from the wallet
-    const teamDetails = await getSafeDetails(project.multisigController as Address);
+    const contractReceipt = await txResponse.transactionResponse?.wait();
 
-    //Send authorizers a mail
-    for (let index = ZERO; index < teamDetails.signers.length; index++) {
+    if (!contractReceipt) {
+        throw new Error("Error approving transaction");
+    }
 
-        const signer = teamDetails.signers[index];
-
-        const authorizationRequestId = getConcatenatedId(signer.userId, transaction.id);
-
-        const authorizationRequest = await authorizationRequestRepository().create(authorizationRequestId, {
-            signer: signer.userId,
-            transaction: transaction.id,
-            status: AuthorizationRequest.Pending
-        });
-
-        const link = `${process.env.HOST_LINK}/authorization?projectId=${projectId}&requestId=${authorizationRequest.id}`;
-
-        const mailOptions = getAuthorizationRequestMailOption(
-            {
-                email: signer.email,
-                firstname: signer.firstname,
-                projectName: project.name,
-                assetName: asset.name,
-                link
-            });
-
-        (await MailService.getTransporter()).sendMail(mailOptions, (error) => {
-            if (error) {
-                throw new Error("An Error occurred\nNo email sent!");
-            }
-        });
-    };
+    return { txHash: safeTxHash, nonce };
 }
