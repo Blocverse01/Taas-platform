7c0d732176e3765bea0beb7f52272c056eaf119f
Avoaja Joshua Akachi <avoajajoshua@gmail.com>
2023-10-21 20:50:53 +0100

    feat: setup solidity workspace

--- solidity/hardhat.config.ts
+++ solidity/hardhat.config.ts
@@ -2,19 +2,25 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import { config } from "dotenv";
 
-config();
+config({
+  path: "../.env"
+});
 
 const configuration: HardhatUserConfig = {
   solidity: "0.8.20",
   paths: {
-    sources: "./solidity/contracts",
+    sources: "./contracts",
     tests: "./test",
     cache: "./cache",
-    artifacts: "./solidity/artifacts"
+    artifacts: "./artifacts"
   },
   networks: {
     mumbai: {
       url: `https://rpc.ankr.com/polygon_mumbai`,
+      accounts: [`${process.env.SECRET_KEY}`],
+    },
+    polygon_mainnet: {
+      url: `https://rpc.ankr.com/polygon`,
       accounts: [`${process.env.SECRET_KEY}`],
     },
     optimism_goerli: {
