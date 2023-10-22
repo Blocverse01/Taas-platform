4f3e37a7088d11e69c7595eec6093b319b0459b3
king-Alex-d-great <ogubuikealex@gmail.com>
2023-10-22 07:47:15 +0100

    Feat: Add create project API Handler

---
+++ src/pages/api/project/create-project.ts
@@ -0,0 +1,41 @@
+import { HttpError } from "@/lib/errors";
+import { createNewProject } from "@/lib/taas-api/project/addProject";
+import { storeProjectItem } from "@/pages/dashboard/projects/projectIntegration";
+import { validateAuthInApiHandler } from "@/utils/auth";
+import { BAD_REQUEST, INTERNAL_SERVER_ERROR, OK, UNAUTHORIZED } from "@/utils/constants";
+import { NextApiHandler, NextApiRequest, NextApiResponse } from "next";
+
+const handler: NextApiHandler = async (req: NextApiRequest, res: NextApiResponse) => {
+    try {
+        const session = await validateAuthInApiHandler(req, res);
+
+        if (!session) {
+            res.status(UNAUTHORIZED).send({});
+        }
+
+        const userId = session.user.id;
+
+        const { name, blockchain, treasuryWallet } = req.body;
+
+        if (!name.trim() || !blockchain || !treasuryWallet) {
+            throw new HttpError(BAD_REQUEST, "Invalid Body Properties");
+        }
+
+        await createNewProject({
+            name,
+            blockchain,
+            treasuryWallet,
+            userId,
+            assetType: "real estate"
+        });
+
+        res.status(OK).json({ message: "Project Created Successfully" });
+    } catch (error: any) {
+
+        return res
+            .status(error?.status ?? error?.response?.status ?? INTERNAL_SERVER_ERROR)
+            .json({ message: error.message });
+    }
+}
+
+export default handler;
