0f4db8e38a59c204982383a3c72331ad6c75f57d
Avoaja Joshua Akachi <avoajajoshua@gmail.com>
2023-10-22 14:15:46 +0100

    feat: set z-index on landing page content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,16 +3,7 @@ import { Navbar } from "@/components/landingPage/navbar";
 import Image from "next/image";
 import hero from "@/assets/hero-img.png";
 import heroRaw from "@/assets/hero-img-raw.png";
-import {
-  Facebook,
-  Twitter,
-  Instagram,
-  Linkedln,
-  Plus,
-  Tick,
-  Tick2,
-  HeroBg,
-} from "@/assets/icon";
+import { Facebook, Twitter, Instagram, Linkedln, Plus, Tick, Tick2, HeroBg } from "@/assets/icon";
 import { ComponentProps } from "react";
 import {
   CustomPlan,
@@ -22,6 +13,7 @@ import {
   TeamManagement,
   TeamMemberPlan,
 } from "@/components/landingPage/sectionData";
+import Link from "next/link";
 
 type MenuItems = ComponentProps<typeof Navbar>["menuItems"]; // MenuItem[];
 
@@ -61,9 +53,7 @@ export default function Home() {
     <div className="flex flex-col space-y-4">
       <div className="mx-auto w-fit">{icon}</div>
       <h2 className="text-[24px] text-center font-medium">{title}</h2>
-      <p className="xl:max-w-[383px] xl:ml-4 text-center text-base opacity-70">
-        {text}
-      </p>
+      <p className="xl:max-w-[383px] xl:ml-4 text-center text-base opacity-70">{text}</p>
     </div>
   );
 
@@ -77,10 +67,10 @@ export default function Home() {
 
   return (
     <div>
-      <div className="absolute opacity-20 flex justify-center inset-0 top-[12px]">
+      <div className="absolute opacity-20 flex z-1 justify-center inset-0 top-[12px]">
         <HeroBg />
       </div>
-      <div className="max-w-[1440px] mx-auto">
+      <div className="max-w-[1440px] z-50 relative mx-auto">
         <Navbar menuItems={menuItems} isLoggedIn={false} />
         <main className="lg:mt-[100px] mt-20 overflow-x-hidden text-t-black">
           <section className="text-center px-6 md:px-12 xl:px-[102px] flex flex-col relative">
@@ -91,15 +81,16 @@ export default function Home() {
               Unlock The Power of Asset Tokenization
             </h1>
             <p className="text-[24px] text-center leading-[36px] max-w-[733px] mx-auto mt-8 text-t-black">
-              On Taas, we enable developers create and manage tokenized assets
-              while securing accounts and monitoring trading metrics.
+              On Taas, we enable developers create and manage tokenized assets while securing accounts and
+              monitoring trading metrics.
             </p>
-            <button
+            <Link
+              href="/signup"
               type="button"
               className="w-fit mx-auto hover:-translate-y-[3px] mt-8 duration-200 font-medium bg-t-purple text-white rounded-lg text-[20px] py-4 px-[36.75px] shadow-[0px_2px_4px_0px_rgba(0,0,0,0.25)]"
             >
               Get Started
-            </button>
+            </Link>
 
             <div className="relative z-50 mt-[100px]">
               <Image
@@ -118,11 +109,7 @@ export default function Home() {
             <div className="grid grid-cols-1 gap-8 mt-16  md:grid-cols-3">
               {ManageAssets.map((item) => (
                 <div key={item.id}>
-                  <Features
-                    title={item.title}
-                    text={item.text}
-                    icon={item.icon}
-                  />
+                  <Features title={item.title} text={item.text} icon={item.icon} />
                 </div>
               ))}
             </div>
@@ -144,11 +131,7 @@ export default function Home() {
             <div className="grid grid-cols-1 gap-8 mt-16  md:grid-cols-3">
               {TeamManagement.map((item) => (
                 <div key={item.id}>
-                  <Features
-                    title={item.title}
-                    text={item.text}
-                    icon={item.icon}
-                  />
+                  <Features title={item.title} text={item.text} icon={item.icon} />
                 </div>
               ))}
             </div>
@@ -164,31 +147,29 @@ export default function Home() {
               <div className="bg-t-purple/20 hover:-translate-y-[3px] duration-200 p-7 rounded-xl ">
                 <p className=" text-2xl ">Startup Investment</p>
                 <p className="text-base mt-2 leading-[28px] opacity-70">
-                  TAAS offers the opportunity to create tokens for your startup
-                  investment scheme. It’s comes in really handy for new startups
-                  looking to raise funds.{" "}
+                  TAAS offers the opportunity to create tokens for your startup investment scheme. It’s comes
+                  in really handy for new startups looking to raise funds.{" "}
                 </p>
               </div>
               <div className="border hover:-translate-y-[3px] duration-200 border-t-gray-12 p-7 rounded-xl ">
                 <p className=" text-2xl ">Real Estate</p>
                 <p className="text-base mt-2 leading-[28px] opacity-70">
-                  Create and manage tokens for your real estate projects with
-                  ease, enabling fractional liquidity and ownership for property
-                  investments.
+                  Create and manage tokens for your real estate projects with ease, enabling fractional
+                  liquidity and ownership for property investments.
                 </p>
               </div>
               <div className="bg-t-purple/20 p-7 hover:-translate-y-[3px] duration-200 rounded-xl">
                 <p className=" text-2xl ">Blue/Green Bonds</p>
                 <p className="text-base mt-2 leading-[28px] opacity-70">
-                  TAAS helps you support sustainable projects, and invest in
-                  blue or green bonds with digital tokens.
+                  TAAS helps you support sustainable projects, and invest in blue or green bonds with digital
+                  tokens.
                 </p>
               </div>
               <div className="border border-t-gray-12 p-7 hover:-translate-y-[3px] duration-200 rounded-xl ">
                 <p className=" text-2xl ">Antiques</p>
                 <p className="text-base mt-2 leading-[28px] opacity-70">
-                  On TAAS, you are able to diversify your portfolio, and trade
-                  shares in valuable antiques as digital tokens.
+                  On TAAS, you are able to diversify your portfolio, and trade shares in valuable antiques as
+                  digital tokens.
                 </p>
               </div>
             </div>
@@ -201,8 +182,8 @@ export default function Home() {
                   Seamless Integration for Developers
                 </h2>
                 <p className="xl:w-auto md:w-[360px] text-xl md:text-[20px] mt-4 text-t-black opacity-70 leading-[32px] max-w-[700px]">
-                  On Taas, we enable developers create and manage tokenized
-                  assets while securing accounts and monitoring trading metrics.
+                  On Taas, we enable developers create and manage tokenized assets while securing accounts and
+                  monitoring trading metrics.
                 </p>
               </div>
               <div>
@@ -218,11 +199,7 @@ export default function Home() {
             <div className="grid grid-cols-1 gap-y-12 md:gap-y-0 md:gap-x-12 mt-16 md:grid-cols-3">
               {DevIntegrations.map((item) => (
                 <div key={item.id}>
-                  <Integrations
-                    title={item.title}
-                    text={item.text}
-                    icon={item.icon}
-                  />
+                  <Integrations title={item.title} text={item.text} icon={item.icon} />
                 </div>
               ))}
             </div>
@@ -235,9 +212,7 @@ export default function Home() {
             />
             <div className="grid grid-cols-1 gap-y-8 mt-10 md:grid-cols-2 xl:grid-cols-3 md:mt-16 md:gap-8 lg:gap-x-[48px]">
               <div className="flex flex-col w-full bg-white rounded-[12px] space-y-8 px-[26px] py-10">
-                <h2 className="text-[20px] font-medium text-t-purple">
-                  Team Member Plan
-                </h2>
+                <h2 className="text-[20px] font-medium text-t-purple">Team Member Plan</h2>
                 <button className="select-none hover:-translate-y-[3px] text-white bg-t-purple duration-200 gradient-bg rounded-lg text-base font-medium py-[18px] px-10">
                   Get Started
                 </button>
@@ -247,18 +222,13 @@ export default function Home() {
                       <p className="w-5 h-5 justify-center rounded-full flex items-center bg-t-purple">
                         <Tick />
                       </p>
-                      <span className="ml-3 text-[18px] font-medium">
-                        {" "}
-                        {plan.plan}
-                      </span>
+                      <span className="ml-3 text-[18px] font-medium"> {plan.plan}</span>
                     </div>
                   </div>
                 ))}
               </div>
               <div className="flex flex-col bg-t-purple w-full px-[26px] py-10 rounded-[12px] space-y-8">
-                <h2 className="text-[20px] font-medium text-white">
-                  Property Listing Plan
-                </h2>
+                <h2 className="text-[20px] font-medium text-white">Property Listing Plan</h2>
                 <button className="select-none hover:-translate-y-[3px] text-t-purple bg-white  duration-200  gradient-bg rounded-lg text-base font-medium py-[18px] px-10">
                   Get Started
                 </button>
@@ -268,18 +238,13 @@ export default function Home() {
                       <p className="w-5 h-5 justify-center rounded-full flex items-center bg-white">
                         <Tick2 />
                       </p>
-                      <span className="ml-3 text-[18px] font-medium">
-                        {" "}
-                        {plan.plan}
-                      </span>
+                      <span className="ml-3 text-[18px] font-medium"> {plan.plan}</span>
                     </div>
                   </div>
                 ))}
               </div>
               <div className="flex flex-col space-y-8 w-full px-[26px] py-10 rounded-[12px] bg-white">
-                <h2 className="text-[20px] font-medium text-t-purple">
-                  Custom
-                </h2>
+                <h2 className="text-[20px] font-medium text-t-purple">Custom</h2>
                 <button className="select-none hover:-translate-y-[3px] text-white  duration-200  bg-t-purple rounded-lg text-base font-medium py-[18px] px-10">
                   Contact Us
                 </button>
@@ -289,10 +254,7 @@ export default function Home() {
                       <p className="w-5 h-5 justify-center rounded-full flex items-center bg-t-purple">
                         <Tick />
                       </p>
-                      <span className="ml-3 text-[18px] font-medium">
-                        {" "}
-                        {plan.plan}
-                      </span>
+                      <span className="ml-3 text-[18px] font-medium"> {plan.plan}</span>
                     </div>
                   </div>
                 ))}
@@ -306,8 +268,7 @@ export default function Home() {
                   Tokenize your assets in another dimension
                 </h2>
                 <p className="text-base max-w-[384px] mt-3 opacity-70 leading-[140%]">
-                  Let us help you create and manage your tokenized assets with
-                  extraordinary ease.
+                  Let us help you create and manage your tokenized assets with extraordinary ease.
                 </p>
               </div>
               <div>
@@ -335,9 +296,7 @@ export default function Home() {
                   <Linkedln />
                 </a>
               </div>
-              <p className="text-sm text-gray-800">
-                © 2023 TAAS. All rights reserved.
-              </p>
+              <p className="text-sm text-gray-800">© 2023 TAAS. All rights reserved.</p>
               <h2 className="font-bold text-2xl text-t-purple">TAAS</h2>
             </footer>
           </section>
