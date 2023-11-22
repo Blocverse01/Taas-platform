d030589960f203691b51fb889f869d58d540d517
king-Alex-d-great <ogubuikealex@gmail.com>
2023-11-22 12:00:17 +0100

    Feat: Add Get Asset Functionality

--- src/lib/taas-api/token/getTokenInformation.ts
+++ src/lib/taas-api/token/getTokenInformation.ts
@@ -2,6 +2,7 @@ import { Address } from "viem";
 import { ASSET_TOKEN } from "@/utils/web3/abis";
 import { getPublicClient } from "@/utils/web3/connection";
 import { utils } from "@/utils/web3/utils";
+import { assetPropertyRepository } from "@/utils/constants";
 
 const getTokenInformation = async (tokenAddress: Address) => {
     const publicClient = getPublicClient();
@@ -17,4 +18,13 @@ const getTokenInformation = async (tokenAddress: Address) => {
     return { totalSupply: formattedTotalSupply };
 };
 
-export { getTokenInformation };
+const getAsset = async (tokenAddress: Address) => {
+
+    const assetToken = await assetPropertyRepository().filter({
+        tokenAddress
+    }).getFirstOrThrow();
+
+    return assetToken;
+};
+
+export { getTokenInformation, getAsset };
