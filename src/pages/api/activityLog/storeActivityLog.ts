e713300ade3867946ec0eb85fca5726a35a9423c
Avoaja Joshua Akachi <avoajajoshua@gmail.com>
2023-11-28 10:28:52 +0100

    feat: update response message

--- src/pages/api/activityLog/storeActivityLog.ts
+++ src/pages/api/activityLog/storeActivityLog.ts
@@ -4,25 +4,30 @@ import { validateAuthInApiHandler } from "@/utils/auth";
 import { BAD_REQUEST, INTERNAL_SERVER_ERROR, OK } from "@/utils/constants";
 import { NextApiHandler, NextApiRequest, NextApiResponse } from "next";
 
-const handler: NextApiHandler = async (req: NextApiRequest, res: NextApiResponse) => {
-    try {
-        await validateAuthInApiHandler(req, res);
+const handler: NextApiHandler = async (
+  req: NextApiRequest,
+  res: NextApiResponse
+) => {
+  try {
+    await validateAuthInApiHandler(req, res);
 
-        const { projectId, logItem } = req.body.projectId;
+    const { projectId, logItem } = req.body;
 
-        if (!projectId || !projectId.trim() || !logItem) {
-            throw new HttpError(BAD_REQUEST, "projectId is required");
-        }
-
-        const log = await storeProjectActivityLogItem(projectId, logItem);
-
-        res.status(OK).json({ message: "Project Logs Stored Successfully", data: log });
-    } catch (error: any) {
-        console.log(error);
-        return res
-            .status(error?.status ?? error?.response?.status ?? INTERNAL_SERVER_ERROR)
-            .json({ message: error.message });
+    if (!projectId || !projectId.trim() || !logItem) {
+      throw new HttpError(BAD_REQUEST, "request body is invalid");
     }
-}
+
+    const log = await storeProjectActivityLogItem(projectId, logItem);
+
+    res
+      .status(OK)
+      .json({ message: "Project Logs Stored Successfully", data: log });
+  } catch (error: any) {
+    console.log(error);
+    return res
+      .status(error?.status ?? error?.response?.status ?? INTERNAL_SERVER_ERROR)
+      .json({ message: error.message });
+  }
+};
 
 export default handler;
