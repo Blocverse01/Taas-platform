b7373f110c27e8b4f3397f255a22228ad8d8b117
Avoaja Joshua Akachi <avoajajoshua@gmail.com>
2024-03-04 11:59:47 +0000

    feat: add page loader

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,11 +4,13 @@ import type { NextPage } from 'next';
 import type { AppProps } from 'next/app';
 import { aeonikFont } from '@/font/setup';
 import 'react-loading-skeleton/dist/skeleton.css';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useSetModalParent } from '@/data/store/zustand/modalSlice';
 import { Toaster } from 'react-hot-toast';
 import { TAAS_PURPLE } from 'tailwind.config';
 import { SessionProvider } from 'next-auth/react';
+import { useRouter } from 'next/router';
+import { GridLoader } from 'react-spinners';
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement<P>) => ReactNode;
@@ -23,30 +25,63 @@ export default function App({
   pageProps: { session, ...pageProps },
 }: AppPropsWithLayout) {
   const modalRef = useRef<HTMLDivElement | null>(null);
+
   const setModalParent = useSetModalParent();
-  const getLayout = Component.getLayout ?? ((page) => page);
+
+  const router = useRouter();
+
+  const [pageIsLoading, setPageIsLoading] = useState(false);
+
+  useEffect(() => {
+    const handleRouteChange = (_: unknown) => {
+      setPageIsLoading(true);
+    };
+
+    const handleRouteChangeComplete = () => {
+      setPageIsLoading(false);
+    };
+
+    router.events.on('routeChangeStart', handleRouteChange);
+    router.events.on('routeChangeComplete', handleRouteChangeComplete);
+
+    return () => {
+      router.events.off('routeChangeStart', handleRouteChange);
+      router.events.off('routeChangeComplete', handleRouteChangeComplete);
+    };
+  }, [router.events]);
+
   useEffect(() => {
     if (modalRef.current) {
       setModalParent(modalRef.current);
     }
   }, [setModalParent]);
 
+  const getLayout = Component.getLayout ?? ((page) => page);
+
   return (
-    <SessionProvider session={session}>
-      <div ref={modalRef} className={`${aeonikFont.variable} font-aeonik`}>
-        <Toaster
-          toastOptions={{
-            className: 'border-t-purple border p-4 text-t-purple',
-            success: {
-              iconTheme: {
-                primary: TAAS_PURPLE,
-                secondary: 'white',
+    <div>
+      {pageIsLoading && (
+        <div className="fixed inset-0 z-[9999] h-full w-full flex items-center justify-center bg-white/50">
+          <GridLoader color={TAAS_PURPLE} />
+        </div>
+      )}
+
+      <SessionProvider session={session}>
+        <div ref={modalRef} className={`${aeonikFont.variable} font-aeonik`}>
+          <Toaster
+            toastOptions={{
+              className: 'border-t-purple border p-4 text-t-purple',
+              success: {
+                iconTheme: {
+                  primary: TAAS_PURPLE,
+                  secondary: 'white',
+                },
               },
-            },
-          }}
-        />
-        {getLayout(<Component {...pageProps} />)}
-      </div>
-    </SessionProvider>
+            }}
+          />
+          {getLayout(<Component {...pageProps} />)}
+        </div>
+      </SessionProvider>
+    </div>
   );
 }
